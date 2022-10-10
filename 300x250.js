4799d8b034148bb6c754d53424c913649a8a41e7
Agus <55262772+agusyasin@users.noreply.github.com>
2022-10-10 22:09:23 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9954a4805536150c7c26e1cd9f70a7ea',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://protectedfolkssomebody.com/9954a4805536150c7c26e1cd9f70a7ea/invoke.js"></scr' + 'ipt>');
